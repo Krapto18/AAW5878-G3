4fdad0b64abe59dba41fb5e20a259d5af517b64c
Kiara <U202313457@upc.edu.pe>
2025-09-15 23:08:48 -0500

    Dto Linked Profile

---
+++ KidTD/src/main/java/pe/edu/upc/kidtd/dtos/LinkedProfileDTO.java
@@ -0,0 +1,53 @@
+package pe.edu.upc.kidtd.dtos;
+
+import jakarta.persistence.*;
+import pe.edu.upc.kidtd.entities.User;
+
+public class LinkedProfileDTO {
+
+    private int linkId;
+    private User tutor;
+    private User child;
+    private User professional;
+    private Boolean status;
+
+    public int getLinkId() {
+        return linkId;
+    }
+
+    public void setLinkId(int linkId) {
+        this.linkId = linkId;
+    }
+
+    public User getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(User tutor) {
+        this.tutor = tutor;
+    }
+
+    public User getChild() {
+        return child;
+    }
+
+    public void setChild(User child) {
+        this.child = child;
+    }
+
+    public User getProfessional() {
+        return professional;
+    }
+
+    public void setProfessional(User professional) {
+        this.professional = professional;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
