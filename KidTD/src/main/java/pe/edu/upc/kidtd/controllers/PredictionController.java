0e7a3de088a2ac0b17fadc99c2ab44982dd143ea
Kiara <U202313457@upc.edu.pe>
2025-10-05 17:14:44 -0500

    QUERY 1

--- KidTD/src/main/java/pe/edu/upc/kidtd/controllers/PredictionController.java
+++ KidTD/src/main/java/pe/edu/upc/kidtd/controllers/PredictionController.java
@@ -5,10 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.kidtd.dtos.PrediccionPromedioDTO;
 import pe.edu.upc.kidtd.dtos.PredictionDTO;
 import pe.edu.upc.kidtd.entities.Prediction;
 import pe.edu.upc.kidtd.servicesinterfaces.IPredictionService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -29,5 +31,25 @@ public class PredictionController {
         ModelMapper m = new ModelMapper();
         Prediction prediction=m.map(predictionDTO, Prediction.class);
         predictionService.insert(prediction);
+    }
+    @GetMapping("/query1")
+    public ResponseEntity<?> totalSoftware() {
+        List<PrediccionPromedioDTO> qDto = new ArrayList<>();
+        List<String[]> lic = predictionService.PrediccionPromedio();
+
+        if (lic.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("No hay data");
+        }
+
+        for (String[] columna:lic) {
+
+            PrediccionPromedioDTO dto = new PrediccionPromedioDTO();
+            dto.setMes(columna[0]);
+            dto.setTotalPredicciones(Integer.parseInt(columna[1]));
+            dto.setScorePromedioMensual(Double.parseDouble(columna[2]));
+            qDto.add(dto);
+        }
+        return ResponseEntity.ok(qDto);
     }
 }
