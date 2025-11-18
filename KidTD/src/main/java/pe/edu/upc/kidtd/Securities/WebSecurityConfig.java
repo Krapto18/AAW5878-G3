f3d21752a87a7898cb8fe08a539783c2e74bb783
u202314539 <u202314539@upc.edu.pe>
2025-11-18 11:12:57 -0500

    Seguridad quitado del Questions Controller y agregada la ruta de /Questions en el webconfig

--- KidTD/src/main/java/pe/edu/upc/kidtd/Securities/WebSecurityConfig.java
+++ KidTD/src/main/java/pe/edu/upc/kidtd/Securities/WebSecurityConfig.java
@@ -61,7 +61,7 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .requestMatchers("/login", "/Users/**", "Questionnaries/**", "/Roles/**", "/Goal/**", "/SymptomsLog/**").permitAll()
+                        .requestMatchers("/login", "/Users/**", "Questionnaries/**", "/Roles/**", "/Goal/**", "/SymptomsLog/**", "/Questions/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
